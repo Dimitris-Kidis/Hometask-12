57414f1878ae62e5f0f7ed36d7731339e3dcd7ea
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-04 14:14:50 +0300

    Event Minor Changes

--- Hometask 12/Program.cs
+++ Hometask 12/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
-// https://code-maze.com/csharp-events/
+
+
+// 1. Create a program using events
+//     - (1) Define type
+//     - (2) Define event member
+//     - (3) Define a notifying method
+//     - (4) Define a method triggering an event
 
 namespace App
 {
@@ -12,13 +18,15 @@ namespace App
             var orderingService = new FoodOrderingService();
             var appService = new AppService();
             var mailService = new MailService();
+            var fileService = new FileService();
 
-            orderingService.AddSubscriber(appService);
-            orderingService.AddSubscriber(mailService);
+            orderingService.Subscribe(appService);
+            orderingService.Subscribe(mailService);
+            orderingService.Subscribe(fileService);
 
             orderingService.PrepareOrder(order);
 
-            orderingService.RemoveSubscriber(appService);
+            orderingService.Unsubscribe(appService);
             orderingService.PrepareOrder(order);
 
 
@@ -35,13 +43,13 @@ namespace App
 
     public interface IService
     {
-        void OnFoodPrepared(object source, FoodPreparedEventArgs eventArgs);
+        void FoodPrepareHandler(object source, FoodPreparedEventArgs eventArgs);
     }
 
 
     public class AppService : IService
     {
-        public void OnFoodPrepared(object source, FoodPreparedEventArgs eventArgs)
+        public void FoodPrepareHandler(object source, FoodPreparedEventArgs eventArgs)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"AppService: your food '{eventArgs.Order.Item}' is prepared.");
@@ -51,13 +59,37 @@ namespace App
 
     public class MailService : IService
     {
-        public void OnFoodPrepared(object source, FoodPreparedEventArgs eventArgs)
+        public void FoodPrepareHandler(object source, FoodPreparedEventArgs eventArgs)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"MailService: your food '{eventArgs.Order.Item}' is prepared.");
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
+
+    public class FileService : IService
+    {
+        public void FoodPrepareHandler(object source, FoodPreparedEventArgs eventArgs)
+        {
+            try
+            {
+                FileStream fileStream = File.Open("OrderLog.txt",
+                    FileMode.Append, FileAccess.Write);
+
+                StreamWriter fileWriter = new StreamWriter(fileStream);
+
+                fileWriter.WriteLine(eventArgs.Order.Item + " ordered at " + System.DateTime.Now.ToString() + " by " + Environment.UserName + " on " + Environment.OSVersion);
+                fileWriter.Flush();
+                fileWriter.Close();
+
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe);
+            }
+        }
+    }
+
 
     // 1.
     public class FoodPreparedEventArgs : EventArgs
@@ -70,24 +102,25 @@ namespace App
     public class FoodOrderingService
     {
         // 2.
-        public delegate void FoodPreparedEventHandler(object source, FoodPreparedEventArgs args);
-        private event FoodPreparedEventHandler FoodPrepared;
+        //public delegate void FoodPreparedEventHandler(object source, FoodPreparedEventArgs args);
+        //private event FoodPreparedEventHandler FoodPrepared;
+        private event EventHandler<FoodPreparedEventArgs> FoodPreparedEventHandler = default!;
 
-        public void AddSubscriber(IService service)
+        public void Subscribe(IService service)
         {
-            FoodPrepared += service.OnFoodPrepared;
+            FoodPreparedEventHandler += service.FoodPrepareHandler;
         }
 
-        public void RemoveSubscriber(IService service)
+        public void Unsubscribe(IService service)
         {
-            FoodPrepared -= service.OnFoodPrepared;
+            FoodPreparedEventHandler -= service.FoodPrepareHandler;
         }
 
         // 3.
         protected virtual void OnFoodPrepared(Order order)
         {
-            if (FoodPrepared != null)
-                FoodPrepared(this, new FoodPreparedEventArgs { Order = order });
+            if (FoodPreparedEventHandler != null)
+                FoodPreparedEventHandler(this, new FoodPreparedEventArgs { Order = order });
         }
 
         // 4.
